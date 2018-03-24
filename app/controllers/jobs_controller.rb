9672ecf1f14193fd1a0ce35dcc2240826b4f66bd
Railsbridge Student <unconfigured-student@railsbridge.local>
2018-03-24 15:58:04 +0000

    Add CSS to app/assets/stylesheets

---
+++ app/controllers/jobs_controller.rb
@@ -0,0 +1,23 @@
+class JobsController < ApplicationController
+
+  def index
+    @jobs = Job.all
+  end
+
+  def new
+    @job = Job.new
+  end
+
+  def create
+    p "In the create method!!!!!!"
+    job = Job.create(job_params)
+    p job
+    redirect_to jobs_path
+  end
+
+  private
+
+  def job_params
+    params.require(:job).permit(:title, :description)
+  end
+end
